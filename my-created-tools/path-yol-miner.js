9796d13a8a308449689851bc59409d75e429087f
Adem süslü <84082893+ademsuslu@users.noreply.github.com>
2025-09-11 12:41:38 +0300

    Create path-yol-miner.js

---
+++ my-created-tools/path-yol-miner.js
@@ -0,0 +1,34 @@
+javascript: (function() {
+    var scripts = document.getElementsByTagName("script"),
+        regex = /(?<=(\"|\%27|\`))\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\%60))/g;
+    const results = new Set;
+    for (var i = 0; i < scripts.length; i++) {
+        var t = scripts[i].src;
+        "" != t && fetch(t).then(function(t) {
+            return t.text()
+        }).then(function(t) {
+            var e = t.matchAll(regex);
+            for (let r of e) results.add(r[0])
+        }).catch(function(t) {
+            console.log("An error occurred: ", t)
+        })
+    }
+    var pageContent = document.documentElement.outerHTML,
+        matches = pageContent.matchAll(regex);
+    for (const match of matches) results.add(match[0]);
+
+    function writeResults() {
+        var newTab = window.open("", "_blank");
+        if (newTab) {
+            results.forEach(function(t) {
+                newTab.document.write(t + "<br>");
+            });
+            newTab.document.close();
+        } else {
+            results.forEach(function(t) {
+              document.write(t + "<br>");
+            })
+        }
+    }
+    setTimeout(writeResults, 3e3);
+})();
